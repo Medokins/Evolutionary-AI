9952684797de1d682c290bb630e819942828fa15
Medokins <nikodemszwast@gmail.com>
2022-05-07 14:43:20 +0200

    add level drawing

--- game/main.py
+++ game/main.py
@@ -1,24 +1,28 @@
-import pygame
+import pygame, sys
 from settings import *
+from level import Level
+from player import Player, PLAYER_IMG
 
-pygame.display.set_caption("Jump King")
+pygame.init()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+level = Level(level_map, WIN)
 
 def draw_window(x, y):
-    WIN.fill((255,255,255))
+    WIN.fill((26,26,26))
     WIN.blit(PLAYER_IMG, (x, y))
+    level.run()
     pygame.display.update()
 
 def main():
-    run = True
-    clock = pygame.time.Clock()
     jumpKing = Player(960, 540)
 
-    while run:
-        clock.tick(FPS)
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit()
+                sys.exit()
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and not jumpKing.isJumping:
                     channel_time = pygame.time.get_ticks()
@@ -36,45 +40,9 @@ def main():
 
         if jumpKing.isJumping:
             jumpKing.jump(jump_height)
-
-        draw_window(jumpKing.x, jumpKing.y)   
-
-    pygame.quit()
-
-class Player:
-    IMGS = PLAYER_IMGS
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.vel_y = 10
-        self.vel_x = 10
-        self.isJumping = False
-
-
-    def jump(self, jump_height):      
-        '''
-        makes player jump when spacebar is pressed, height of jump is controlled by the time space bar is being pressed
-        ''' 
-        self.y -= self.vel_y * jump_height
-        self.vel_y -= 0.5
-        if self.vel_y < -10:
-            self.vel_y = 10
-            self.isJumping = False
-            jump_height = 0
-
-    
-    def moveRight(self):
-        '''
-        makes player move right when right arrow or "d" key is pressed
-        '''     
-        self.x += self.vel_x
-
-    def moveLeft(self):
-        '''
-        makes player move left when left arrow or "a" key is pressed
-        '''       
-        self.x -= self.vel_x
+            
+        draw_window(jumpKing.x, jumpKing.y)
+        clock.tick(FPS)
 
 if __name__ == "__main__":
     main()
