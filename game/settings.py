4127af437f53c850273845463ccc86c3953db5ec
Medokins <nikodemszwast@gmail.com>
2022-05-11 17:23:15 +0200

    add settings file

--- game/settings.py
+++ game/settings.py
@@ -1,26 +1,17 @@
-level_map = [
-'                                ',
-'                                ',
-'                                ',
-'                                ',
-'                                ',
-'                                ',
-'          XXXXXX                ',
-'                                ',
-'                                ',
-'                      X         ',
-'                      XXXXX   XX',
-'                      XXXX     X',
-'     X    XXXXXXXXX   XXX       ',
-'    XX                XX        ',
-'   XXX                X         ',
-'  XXXX                X         ',
-' XXXXX                X         ',
-'XXXXXX                X         ']
-
-tile_size = 1080 / len(level_map)
-print(tile_size)
-WIDTH, HEIGHT = 1920, 1080
+# game options/settings
+TITLE = "Jump King"
+WIDTH = 810
+HEIGHT = 1080
 FPS = 60
-JUMP_HEIGHT_MAX = 3
-PLAYER_SIZE = (128,128)
+
+# Player properties
+PLAYER_ACC = 1.2
+PLAYER_FRICTION = -0.2
+PLAYER_GRAV = 0.8
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
+                 (125, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 20)]
