43171f30a9ea206bc2b5c64e1e6f02ac197f7038
Medokins <nikodemszwast@gmail.com>
2022-05-19 15:17:54 +0200

    update settings file

--- game/settings.py
+++ game/settings.py
@@ -1,17 +1,36 @@
 # game options/settings
 TITLE = "Jump King"
-WIDTH = 810
-HEIGHT = 1080
+WIDTH = 480
+HEIGHT = 600
 FPS = 60
+FONT_NAME = 'arial'
+HS_FILE = "highscore.txt"
+SPRITESHEET = "spritesheet_jumper.png"
 
 # Player properties
-PLAYER_ACC = 1.2
-PLAYER_FRICTION = -0.2
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
+
+# Game properties
+PLAYER_LAYER = 2
+PLATFORM_LAYER = 1
+POW_LAYER = 1
 
 # Starting platforms
-PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
-                 (125, HEIGHT - 350, 100, 20),
-                 (350, 200, 100, 20),
-                 (175, 100, 50, 20)]
+PLATFORM_LIST = [(0, HEIGHT - 60),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4 - 50),
+                 (125, HEIGHT - 350),
+                 (350, 200),
+                 (175, 100)]
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
+BGCOLOR = LIGHTBLUE
