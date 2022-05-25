5a94dd82c442697811f20d789837521de831d55d
Medokins <nikodemszwast@gmail.com>
2022-05-25 21:14:16 +0200

    add first function (from TecgWithTim)

--- AI/main.py
+++ AI/main.py
@@ -1,1 +1,29 @@
-#here I will implement evolutionary/genetic AI algorithm
+import os
+import neat
+
+def run(config_file):
+    """
+    runs the NEAT algorithm to train a neural network to play jumo king.
+    :param config_file: location of config file
+    :return: None
+    """
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    #p.add_reporter(neat.Checkpointer(5))
+
+if __name__ == '__main__': 
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)
