3a5e38a33ab2a4d28f69f3a936400e07eaedbb4c
Medokins <nikodemszwast@gmail.com>
2022-06-13 13:26:36 +0200

    decrease jump cooldown

--- AI/sprites.py
+++ AI/sprites.py
@@ -53,7 +53,7 @@ class Player(pg.sprite.Sprite):
         self.highest_platform = 40
         self.previous_highest_platform = 40
         self.last = pg.time.get_ticks()
-        self.cooldown = 1500
+        self.cooldown = 1200
         self.moves = 5
 
         if not AI: self.cooldown = 0
