ca31517123e22a5d0daa96d6939c43eede807f1a
Nikodem <nikodemszwast@gmail.com>
2024-10-11 14:35:07 +0200

    check if token still valid

--- game/sprites.py
+++ game/sprites.py
@@ -20,6 +20,7 @@ class Spritesheet:
         image = pg.Surface((width, height))
         image.blit(self.spritesheet, (0, 0), (x, y, width, height))
         image = pg.transform.scale(image, (width, height))
+        
         return image
 
 class Player(pg.sprite.Sprite):
